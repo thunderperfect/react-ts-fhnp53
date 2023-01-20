a8d5e08b8b49ba5fd36adb3c6940ad42c013f3a7
thunderperfect <michael@method93.com>
2023-01-20 14:14:18 -0500

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,34 +1,38 @@
 import * as React from 'react';
 import './style.css';
-import TreeModel from 'tree-model'
-
+import TreeModel from 'tree-model';
+import ReactJson from 'react-json-view';
 
 export default function App() {
+  let tree = new TreeModel();
 
- let  tree = new TreeModel();
-
-let root = tree.parse({
+  let root = tree.parse({
     id: 1,
     children: [
-        {
-            id: 11,
-            children: [{id: 111}]
-        },
-        {
-            id: 12,
-            children: [{id: 121}, {id: 122}]
-        },
-        {
-            id: 13
-        }
-    ]
-});
+      {
+        id: 11,
+        children: [{ id: 111 }],
+      },
+      {
+        id: 12,
+        children: [{ id: 121 }, { id: 122 }],
+      },
+      {
+        id: 13,
+      },
+    ],
+  });
 
-
+  const updateNode = () => {
+    var node12 = root.first(function (node) {
+      return node.model.id === 12;
+    });
+  };
 
   return (
     <div>
- 
+      <ReactJson src={root.model} />
+      <button onClick={updateNode}>Test</button>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
